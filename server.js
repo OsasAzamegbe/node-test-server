1416e3cc7375a2fba6d8ae6991944f749fd0df6d
osamudiamen <osas.azamegbe@gmail.com>
2020-10-06 21:28:33 +0100

    serving static files for index.html and about.html

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const fs = require('fs')
 const http = require('http')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
+const STATIC_ROOT = path.join(__dirname, 'public')
+
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-type': 'text/html'})
-    res.write('<h1 style="display: flex; justify-content: center;">HOME PAGE</h1>')
-    res.end()
+    if (req.url === '/'){
+        fs.readFile(path.join(STATIC_ROOT, 'index.html'), 'utf-8', (err, data) => {
+            if (err) throw err
+            res.write(data)
+            res.end()
+        })        
+    } else if (req.url === '/about'){
+        fs.readFile(path.join(STATIC_ROOT, 'about.html'), 'utf-8', (err, data) => {
+            if (err) throw err
+            res.write(data)
+            res.end()
+        })        
+    }
 })
 
 server.listen(PORT, () => console.log(`Server Started.\nServer running on port ${PORT}...`))
